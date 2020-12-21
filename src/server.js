77887f0138d5c931aee0fcb8a82b71476133d0fd
Falcao\Kevin <santoskevin032@gmail.com>
2020-12-21 16:50:31 -0300

    feat: add server for heroku

--- src/server.js
+++ src/server.js
@@ -7,3 +7,6 @@ app.use(express.static(__dirname+'/dist/practica2'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/practica2/index.html'));
 });
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
