a039baa0a3e07ec541b414368e81192b18e56f69
Falcao\Kevin <santoskevin032@gmail.com>
2020-12-21 20:50:58 -0300

    feat: ready to heroku

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static(__dirname+'/dist/practica2'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/practica2/index.html'));
 });
 
 console.log("listening...");
